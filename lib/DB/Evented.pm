c6f393df5c759ee390ff1ed856bc9c6fc8064535
Logan Bell <loganbell@gmail.com>
2013-04-24 15:36:35 -0700

    Prepped for CPAN release.
    * Converted to using .rock.yml
    * Converted to using cpanfile
    * Added a dist.ini
    * Updated Changes file
    * Added licensing to module

--- lib/DB/Evented.pm
+++ lib/DB/Evented.pm
@@ -8,13 +8,9 @@ use AnyEvent::DBI;
 
 DB::Evented - A pragmatic DBI like evented module.
 
-=head1 VERSION
-
-Version 0.04
-
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 our $handlers = [];
 
 =head1 SYNOPSIS
@@ -216,7 +212,7 @@ for my $method_name ( qw(selectrow_hashref selectcol_arrayref selectall_hashref 
 # The child processes are technically held by AnyEvent::DBI
 # by clearing the known handlers these children *should* be reaped
 sub DESTROY {
-	my $error = do {
+  my $error = do {
     local $@;
     eval {
       DB::Evented->clear_handlers;
@@ -230,12 +226,6 @@ sub DESTROY {
 
 Logan Bell, C<< <logie at cpan.org> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-shutterstock-db-evented at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DB-Evented>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
@@ -244,70 +234,13 @@ You can find documentation for this module with the perldoc command.
 
 You can also look for information at:
 
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DB-Evented>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/DB-Evented>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/DB-Evented>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/DB-Evented/>
-
-=back
-
 =head1 ACKNOWLEDGEMENTS
 
-Mom, Aaron Cohen, and Belden Lyman and thanks to Shutterstock for allowing me to create this module.
+Aaron Cohen and Belden Lyman.
 
-=head1 LICENSE AND COPYRIGHT
+=head1 LICENSE
 
-Copyright 2013 Logan Bell.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+Copyright (c) 2013 Logan Bell and Shutterstock Inc (http://shutterstock.com).  All rights reserved.  This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =cut
 
